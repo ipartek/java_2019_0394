66e9fed8bc7c5b10de5a4a2459bd3aab75e50821
Javier Lete <jglete@gmail.com>
2019-06-17 12:38:36 +0200

    Recursividad

---
+++ MF0967_3/UF2216/src/com/ipartek/formacion/uf2216/ejemplos/poo/EjemploRecursividad.java
@@ -0,0 +1,26 @@
+package com.ipartek.formacion.uf2216.ejemplos.poo;
+
+import java.math.BigInteger;
+
+public class EjemploRecursividad {
+
+	public static void main(String[] args) {
+//		Vuelta de marcador
+//		long l = Long.MAX_VALUE;
+//		System.out.println(l);
+//		System.out.println(++l);
+		
+		System.out.println(factorial(new BigInteger("5")).toString());
+	}
+	
+	public final static BigInteger UNO = new BigInteger("1");
+	
+	public static BigInteger factorial(BigInteger numero) {
+		if(numero.equals(UNO)) {
+			return numero;
+		}
+		
+		return numero.multiply(factorial(numero.subtract(UNO)));
+	}
+
+}
