10a3034277173bfbdf20e1776a3b867a9b22846b
Javier Lete <jglete@eskura21.com>
2019-07-08 12:13:30 +0200

    AJAX y $.getJSON

---
+++ MF0967_3/UF2217/AJAX/js/index.js
@@ -0,0 +1,43 @@
+/*jslint browser:true, devel:true*/
+/*eslint-env browser, es6*/
+/*eslint no-console: "off"*/
+/*global $*/
+
+$(function () {
+    'use strict';
+    
+    $.getJSON('datos.json', function (datos) {
+        $(datos).each(function () {
+            $('ul').append('<li>' + this.nombre + '</li>');
+        });
+    });
+});
+
+/*
+function porCadaDato() {
+    'use strict';
+    
+    console.log('Dentro de porCadaDato');
+    
+    $('ul').append('<li>' + this.nombre + '</li>');
+}
+
+function recepcionJSON(datos) {
+    'use strict';
+    
+    console.log('Dentro de recepcionJSON');
+    
+    $(datos).each(porCadaDato);
+}
+
+$(function () {
+    'use strict';
+
+    console.log('Antes de getJSON');
+    
+    $.getJSON('datos.json', recepcionJSON);
+    
+    console.log('Después de getJSON');
+});
+
+*/
