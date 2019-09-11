b9d02c4c042c3bee0c843624df05e2f769864c7a
Javier Lete <jglete@gmail.com>
2019-09-11 13:20:55 +0200

    Mejora añadiendo cantidad al diagrama

--- MF0966_3/sql/Ejemplo Facturas, Clientes y Productos.sql
+++ MF0966_3/sql/Ejemplo Facturas, Clientes y Productos.sql
@@ -1,84 +1,273 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ejemplodiagrama
+-- ------------------------------------------------------
+-- Server version	8.0.17
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema ejemplodiagrama
--- -----------------------------------------------------
+--
+-- Current Database: `ejemplodiagrama`
+--
 
--- -----------------------------------------------------
--- Schema ejemplodiagrama
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ejemplodiagrama` DEFAULT CHARACTER SET utf8 ;
-USE `ejemplodiagrama` ;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ejemplodiagrama` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
--- -----------------------------------------------------
--- Table `ejemplodiagrama`.`clientes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ejemplodiagrama`.`clientes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nif` CHAR(9) NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
+USE `ejemplodiagrama`;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nif` char(9) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `nif_UNIQUE` (`nif` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE KEY `nif_UNIQUE` (`nif`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `clientes`
+--
 
--- -----------------------------------------------------
--- Table `ejemplodiagrama`.`facturas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ejemplodiagrama`.`facturas` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `numero` CHAR(9) NOT NULL,
-  `fecha` DATE NOT NULL,
-  `clientes_id` INT NOT NULL,
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'12345678Z','Javier Lete'),(2,'87654321A','Pepe Pérez');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facturas`
+--
+
+DROP TABLE IF EXISTS `facturas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facturas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` char(9) NOT NULL,
+  `fecha` date NOT NULL,
+  `clientes_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `numero_UNIQUE` (`numero` ASC) VISIBLE,
-  INDEX `fk_facturas_clientes_idx` (`clientes_id` ASC) VISIBLE,
-  CONSTRAINT `fk_facturas_clientes`
-    FOREIGN KEY (`clientes_id`)
-    REFERENCES `ejemplodiagrama`.`clientes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE KEY `numero_UNIQUE` (`numero`),
+  KEY `fk_facturas_clientes_idx` (`clientes_id`),
+  CONSTRAINT `fk_facturas_clientes` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `facturas`
+--
 
--- -----------------------------------------------------
--- Table `ejemplodiagrama`.`productos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ejemplodiagrama`.`productos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `descripcion` TEXT(1000) NULL,
-  `precio` DECIMAL(8,2) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `facturas` WRITE;
+/*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
+INSERT INTO `facturas` VALUES (1,'2019/0001','2019-01-02',2),(2,'2019/0002','2019-02-05',1);
+/*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `facturas_has_productos`
+--
 
--- -----------------------------------------------------
--- Table `ejemplodiagrama`.`facturas_has_productos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ejemplodiagrama`.`facturas_has_productos` (
-  `facturas_id` INT NOT NULL,
-  `productos_id` INT NOT NULL,
-  PRIMARY KEY (`facturas_id`, `productos_id`),
-  INDEX `fk_facturas_has_productos_productos1_idx` (`productos_id` ASC) VISIBLE,
-  INDEX `fk_facturas_has_productos_facturas1_idx` (`facturas_id` ASC) VISIBLE,
-  CONSTRAINT `fk_facturas_has_productos_facturas1`
-    FOREIGN KEY (`facturas_id`)
-    REFERENCES `ejemplodiagrama`.`facturas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_facturas_has_productos_productos1`
-    FOREIGN KEY (`productos_id`)
-    REFERENCES `ejemplodiagrama`.`productos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `facturas_has_productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facturas_has_productos` (
+  `facturas_id` int(11) NOT NULL,
+  `productos_id` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT '1',
+  PRIMARY KEY (`facturas_id`,`productos_id`),
+  KEY `fk_facturas_has_productos_productos1_idx` (`productos_id`),
+  KEY `fk_facturas_has_productos_facturas1_idx` (`facturas_id`),
+  CONSTRAINT `fk_facturas_has_productos_facturas1` FOREIGN KEY (`facturas_id`) REFERENCES `facturas` (`id`),
+  CONSTRAINT `fk_facturas_has_productos_productos1` FOREIGN KEY (`productos_id`) REFERENCES `productos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `facturas_has_productos`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `facturas_has_productos` WRITE;
+/*!40000 ALTER TABLE `facturas_has_productos` DISABLE KEYS */;
+INSERT INTO `facturas_has_productos` VALUES (1,1,1),(1,2,1),(2,2,1);
+/*!40000 ALTER TABLE `facturas_has_productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `descripcion` text,
+  `precio` decimal(8,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'Ratón','Ratón super guay',12.34),(2,'Monitor','Se ve muy bien',123.23);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `ejemplos`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ejemplos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `ejemplos`;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Javier Lete'),(2,'Pepe Pérez'),(3,'Javier Lete'),(4,'Nuevo');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facturas`
+--
+
+DROP TABLE IF EXISTS `facturas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facturas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numero_factura` char(9) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_CLIENTES_idx` (`idcliente`),
+  CONSTRAINT `FK_CLIENTES` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facturas`
+--
+
+LOCK TABLES `facturas` WRITE;
+/*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
+INSERT INTO `facturas` VALUES (2,'2019/0001',2),(3,'2019/0002',1),(4,'2019/0003',2);
+/*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `timestamps`
+--
+
+DROP TABLE IF EXISTS `timestamps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `timestamps` (
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `timestamps`
+--
+
+LOCK TABLES `timestamps` WRITE;
+/*!40000 ALTER TABLE `timestamps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `timestamps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(16) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
+  `password` varchar(32) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-11 13:20:47
