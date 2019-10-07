1896f262536dfa316995df071305fd80024b4eb5
Javier Lete <jglete@gmail.com>
2019-10-07 12:29:05 +0200

    Inicialización del proyecto con Spring Boot Starter en Eclipse

---
+++ UF2215/SpringRestBasicoEclipse/src/main/java/com/formacion/ipartek/springrestbasicoeclipse/SpringRestBasicoEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.ipartek.springrestbasicoeclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBasicoEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestBasicoEclipseApplication.class, args);
+	}
+
+}
