ad320a2570098f189d840e338043d6b4b7868cfb
Javier Lete <jglete@gmail.com>
2019-10-07 10:47:58 +0200

    JPA

--- UF2215/hibernate/src/main/java/com/ipartek/formacion/hibernate/App.java
+++ UF2215/hibernate/src/main/java/com/ipartek/formacion/hibernate/App.java
@@ -3,54 +3,39 @@ package com.ipartek.formacion.hibernate;
 import java.util.Date;
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	SessionFactory sessionFactory = pedirSessionFactory();
+    	EntityManagerFactory entityManagerFactory =  Persistence.createEntityManagerFactory( "com.ipartek.formacion.hibernate" );
+    	EntityManager entityManager = entityManagerFactory.createEntityManager();
     	
-    	Session session = sessionFactory.openSession();
+    	entityManager.getTransaction().begin();
+		entityManager.persist( new Event( "Our very first event!", new Date() ) );
+		entityManager.persist( new Event( "A follow up event", new Date() ) );
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		// now lets pull events from the database and list them
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
 		
-		session.beginTransaction();
-		session.save( new Event( "Our very first event!", new Date() ) );
-		// session.getTransaction().commit();
-		// session.beginTransaction();
-		session.save( new Event( "A follow up event", new Date() ) );
-		// session.save( new Event( "A follow up eventtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt", new Date() ) );
-		session.getTransaction().commit();
-		session.close();
+		Event evento = entityManager.find(Event.class, 1L);
+		evento.setTitle("Título modificado");
+		entityManager.persist(evento);
 		
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-		@SuppressWarnings("unchecked")
-		List<Event> result = session.createQuery( "from Event" ).list();
-		for ( Event event : (List<Event>) result ) {
+		evento = entityManager.find(Event.class, 2L);
+		entityManager.remove(evento);
+		
+        List<Event> result = entityManager.createQuery( "from Event", Event.class ).getResultList();
+		for ( Event event : result ) {
 			System.out.println( "Event (" + event.getDate() + ") : " + event.getTitle() );
 		}
-		session.getTransaction().commit();
-		session.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
-
-	private static SessionFactory pedirSessionFactory() {
-    	
-    	final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure() // configures settings from hibernate.cfg.xml
-				.build();
-		try {
-			return new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-		}
-		catch (Exception e) {
-			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-			// so destroy it manually.
-			StandardServiceRegistryBuilder.destroy( registry );
-			throw e;
-			
-		}
-	}
 }
