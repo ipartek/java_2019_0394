63c8c072a0704611cc50ac387e212b6da95cbdf9
Javier Lete <jglete@gmail.com>
2019-10-07 13:09:22 +0200

    Insert con Spring REST Básico

--- UF2215/SpringRestBasicoEclipse/src/main/java/com/formacion/ipartek/springrestbasicoeclipse/ControladorRest.java
+++ UF2215/SpringRestBasicoEclipse/src/main/java/com/formacion/ipartek/springrestbasicoeclipse/ControladorRest.java
@@ -2,9 +2,12 @@ package com.formacion.ipartek.springrestbasicoeclipse;
 
 import java.util.TreeMap;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +15,8 @@ public class ControladorRest {
 	private static TreeMap<Long, Persona> personas = new TreeMap<>();
 
 	static {
-		personas.put(1L, new Persona(1L, "Javier", "Lete"));
-		personas.put(2L, new Persona(2L, "Pepe", "Pérez"));
+		personas.put(1L, new Persona(1L, "Javier", "Lete García"));
+		personas.put(2L, new Persona(2L, "Pepe", "Pérez González"));
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/api/personas")
@@ -24,5 +27,15 @@ public class ControladorRest {
 	@RequestMapping(method = RequestMethod.GET, path = "/api/personas/{id}")
 	public Persona getById(@PathVariable("id") Long id) {
 		return personas.get(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, path = "/api/personas")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Persona insert(@RequestBody Persona persona) {
+		Long id = personas.lastKey() + 1;
+		persona.setId(id);
+		personas.put(id, persona);
+		
+		return persona;
 	}
 }
