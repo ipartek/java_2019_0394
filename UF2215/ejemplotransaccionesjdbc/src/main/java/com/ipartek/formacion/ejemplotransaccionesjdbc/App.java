1b441961b34e21ef300182a0bc3837d1691795b6
Javier Lete <jglete@gmail.com>
2019-10-16 13:56:48 +0200

    Logger de JDK1.4 en adelante

--- UF2215/ejemplotransaccionesjdbc/src/main/java/com/ipartek/formacion/ejemplotransaccionesjdbc/App.java
+++ UF2215/ejemplotransaccionesjdbc/src/main/java/com/ipartek/formacion/ejemplotransaccionesjdbc/App.java
@@ -4,29 +4,42 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class App 
 {
+	static {
+		System.setProperty("java.util.logging.config.file",
+				App.class.getClassLoader().getResource("logging.properties").getFile().replace("%20", " "));
+	}
+	
+	private final static Logger LOGGER = Logger.getLogger("com.ipartek.formacion.ejemplotransaccionesjdbc");
+	
     public static void main( String[] args ) throws SQLException
     {
         String URL = "jdbc:mysql://root:@localhost:3306/banco?serverTimezone=UTC";
         String sql1 = "INSERT INTO movimientos (cuentas_id, concepto, cantidad, divisa) VALUES (1, 'Transacción a 2', -200.5, 'EUR')";
-        String sql2 = "INSERT INTO movimientos (cuentas_id, concepto, cantidad, divisa) VALUES (2, 'Transacción desde 1', 200.5, 'EUR')";
+        String sql2 = "INSERT INTO movimientos (cuentas_id, concepto, cantidad, divisa) VALUES (6, 'Transacción desde 1', 200.5, 'EUR')";
+        
+        LOGGER.info("Inicio de la aplicación");
         
         try (Connection con = DriverManager.getConnection(URL)) {
         	
         	con.setAutoCommit(false);
         	
         	try(Statement stmt = con.createStatement()) {
+        		LOGGER.fine(sql1);
         		stmt.executeUpdate(sql1);
-        		stmt.executeUpdate(sql2);
         		
-        		System.out.println("Finalizado");
+        		LOGGER.fine(sql2);
+        		stmt.executeUpdate(sql2);
         		
         		con.commit();
         		
+        		LOGGER.info("Se ha finalizado la transacción");
+        		
         	} catch(Exception e) {
-        		System.out.println("Error en la operación");
+        		LOGGER.severe("Error en la operación");
         		
         		con.rollback();
         		
