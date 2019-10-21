b9f7b47eff60b8a8997778fcdbc3aed769fc5b86
Javier Lete <jglete@gmail.com>
2019-10-21 09:38:34 +0200

    Ejemplo HelloWorld sin desacoplamiento

---
+++ MF0968_3/springbasico/src/main/java/com/ipartek/formacion/spring/springbasico/App.java
@@ -0,0 +1,9 @@
+package com.ipartek.formacion.spring.springbasico;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
