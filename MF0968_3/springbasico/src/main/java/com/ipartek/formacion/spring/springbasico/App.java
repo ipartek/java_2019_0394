a1a026ed4a5f5625638bcfaa788682ce2e3619ed
Javier Lete <jglete@gmail.com>
2019-10-21 10:42:15 +0200

    Uso de BeanFactory de Spring

--- MF0968_3/springbasico/src/main/java/com/ipartek/formacion/spring/springbasico/App.java
+++ MF0968_3/springbasico/src/main/java/com/ipartek/formacion/spring/springbasico/App.java
@@ -1,14 +1,31 @@
 package com.ipartek.formacion.spring.springbasico;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
-    	MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
-    	
-    	mr.setMessageProvider(mp);
-    	
-    	mr.render();
-    }
+import java.util.Properties;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		BeanFactory factory = getBeanFactory();
+		MessageRenderer mr = (MessageRenderer) factory.getBean("renderer");
+		MessageProvider mp = (MessageProvider) factory.getBean("provider");
+
+		mr.setMessageProvider(mp);
+
+		mr.render();
+	}
+
+	private static BeanFactory getBeanFactory() throws Exception {
+		// get the bean factory
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		// create a definition reader
+		PropertiesBeanDefinitionReader rdr = new PropertiesBeanDefinitionReader(factory);
+		// load the configuration options
+		Properties props = new Properties();
+		props.load(App.class.getResourceAsStream("/springbasico.properties"));
+		rdr.registerBeanDefinitions(props);
+		return factory;
+	}
 }
