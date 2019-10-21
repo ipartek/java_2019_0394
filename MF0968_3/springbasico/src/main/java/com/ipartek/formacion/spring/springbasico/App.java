7d62cb24b9ef54248b89912a33aee5ef074c0d87
Javier Lete <jglete@gmail.com>
2019-10-21 11:40:42 +0200

    Configuración con beans.xml

--- MF0968_3/springbasico/src/main/java/com/ipartek/formacion/spring/springbasico/App.java
+++ MF0968_3/springbasico/src/main/java/com/ipartek/formacion/spring/springbasico/App.java
@@ -1,29 +1,18 @@
 package com.ipartek.formacion.spring.springbasico;
 
-import java.util.Properties;
-
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		BeanFactory factory = getBeanFactory();
+		Resource config = new DefaultResourceLoader().getResource("/beans.xml");
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		new XmlBeanDefinitionReader(factory).loadBeanDefinitions(config);
 		
 		MessageRenderer mr = (MessageRenderer) factory.getBean("renderer");
 		
 		mr.render();
-	}
-
-	private static BeanFactory getBeanFactory() throws Exception {
-		// get the bean factory
-		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-		// create a definition reader
-		PropertiesBeanDefinitionReader rdr = new PropertiesBeanDefinitionReader(factory);
-		// load the configuration options
-		Properties props = new Properties();
-		props.load(App.class.getResourceAsStream("/springbasico.properties"));
-		rdr.registerBeanDefinitions(props);
-		return factory;
 	}
 }
