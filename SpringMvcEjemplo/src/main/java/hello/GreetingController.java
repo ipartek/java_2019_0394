8fe75b8e4c5426e7e2eb5d3d3b89bbaaf9ac6d72
Javier Lete <jglete@gmail.com>
2019-10-21 13:33:42 +0200

    Ejemplo SpringMvc con modelo personalizado

--- SpringMvcEjemplo/src/main/java/hello/GreetingController.java
+++ SpringMvcEjemplo/src/main/java/hello/GreetingController.java
@@ -3,14 +3,14 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@ModelAttribute("persona") Persona persona) {
+        //persona.setNombre(persona.getNombre().toUpperCase());
         return "greeting";
     }
 
---
+++ SpringMvcEjemplo/src/main/java/hello/Persona.java
@@ -0,0 +1,8 @@
+package hello;
+
+import lombok.Data;
+
+public @Data class Persona {
+    private String nombre;
+    private String apellido;
+}
