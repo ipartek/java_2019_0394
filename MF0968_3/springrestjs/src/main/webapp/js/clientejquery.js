7086d2bc8111ae9e7719b34ab72c9ccec7de2b0b
Javier Lete <jglete@gmail.com>
2019-11-05 10:53:54 +0100

    Ejemplo POST con jQuery

--- MF0968_3/springrestjs/src/main/webapp/js/clientejquery.js
+++ MF0968_3/springrestjs/src/main/webapp/js/clientejquery.js
@@ -1,16 +1,51 @@
 'use strict';
 
-$(function() { //Equivalente a window.onload
-	$.getJSON('http://localhost:8080/api/productos', function(respuesta) {
-		var productos = respuesta._embedded.productos;
-		
-		console.log(productos);
-		
-		$(productos).each(function() {
-			console.log(this);
-			
-			$('#productos').append($('<li>').html(
-					this.nombre + ' ' + this.descripcion + ' ' + this.precio));
+var URL = 'http://localhost:8080/api/productos/';
+
+jQuery(function($) { //Equivalente a window.onload
+	$('button').click(function(e) {
+		e.preventDefault(); //Cancelar la emisión del formulario por el action
+
+		var nombre = $('#nombre').val();
+		var descripcion = $('#descripcion').val();
+		var precio = $('#precio').val();
+
+		var producto = { nombre, descripcion, precio };
+
+		console.log(producto);
+
+		$.ajax({
+			url: URL,
+			method: 'POST',
+			data: JSON.stringify(producto),
+			contentType: 'application/json',
+			dataType: 'json'
+		}).done(function(respuesta) {
+			console.log(respuesta);
+			addProductoAListado($, producto);
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			alert(jqXHR.responseJSON.message);
+			console.log(jqXHR, textStatus, errorThrown);
 		});
 	});
+
+	listarProductos($);
 });
+
+function listarProductos($) {
+	$('#productos').empty();
+
+	$.getJSON(URL, function(respuesta) {
+		var productos = respuesta._embedded.productos;
+		console.log(productos);
+		$(productos).each(function() {
+			console.log(this);
+			addProductoAListado($, this);
+		});
+	});
+}
+
+function addProductoAListado($, producto) {
+	$('#productos').append($('<li>').html(
+		producto.nombre + ' ' + producto.descripcion + ' ' + producto.precio));
+}
