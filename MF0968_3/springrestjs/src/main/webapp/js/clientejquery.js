3e920148a8952215572ddb98131c05edef74da4f
Javier Lete <jglete@gmail.com>
2019-11-06 10:54:49 +0100

    Refactorización de jQuery

--- MF0968_3/springrestjs/src/main/webapp/js/clientejquery.js
+++ MF0968_3/springrestjs/src/main/webapp/js/clientejquery.js
@@ -2,50 +2,82 @@
 
 var URL = 'http://localhost:8080/api/productos/';
 
-jQuery(function($) { //Equivalente a window.onload
-	$('button').click(function(e) {
-		e.preventDefault(); //Cancelar la emisión del formulario por el action
+jQuery(alCargarDocumento());
 
-		var nombre = $('#nombre').val();
-		var descripcion = $('#descripcion').val();
-		var precio = $('#precio').val();
+function alCargarDocumento() {
+    return function($) {
+        $('button').click(onClick($));
+        listarProductos($);
+    };
+}
 
+function onClick($) {
+    return function(e) {
+        e.preventDefault(); //Cancelar la emisión del formulario por el action
+
+        var nombre = $('#nombre').val();
+        var descripcion = $('#descripcion').val();
+        var precio = $('#precio').val();
+        
 		var producto = { nombre, descripcion, precio };
 
-		console.log(producto);
+        console.log(producto);
+        
+		var opcionesAjax = {
+            url: URL,
+            method: 'POST',
+            data: JSON.stringify(producto),
+            contentType: 'application/json',
+            dataType: 'json'
+        };
+        
+		var post$ = $.ajax(opcionesAjax);
+		post$.done(onPostCorrecto($, producto));
+		post$.fail(onPostIncorrecto());
+    };
+}
 
-		$.ajax({
-			url: URL,
-			method: 'POST',
-			data: JSON.stringify(producto),
-			contentType: 'application/json',
-			dataType: 'json'
-		}).done(function(respuesta) {
-			console.log(respuesta);
-			addProductoAListado($, producto);
-		}).fail(function(jqXHR, textStatus, errorThrown) {
-			alert(jqXHR.responseJSON.message);
-			console.log(jqXHR, textStatus, errorThrown);
-		});
-	});
+function onPostIncorrecto() {
+    return function(jqXHR, textStatus, errorThrown) {
+        alert(jqXHR.responseJSON.message);
+        console.log(jqXHR, textStatus, errorThrown);
+    };
+}
 
-	listarProductos($);
-});
+function onPostCorrecto($, producto) {
+    return function(respuesta) {
+        console.log(respuesta);
+        addProductoAListado($, producto);
+    };
+}
 
 function listarProductos($) {
 	$('#productos').empty();
 
-	$.getJSON(URL, function(respuesta) {
-		var productos = respuesta._embedded.productos;
-		console.log(productos);
-		$(productos).each(function() {
-			console.log(this);
-			addProductoAListado($, this);
-		});
-	});
+	$.getJSON(URL, onGetCorrecto($));
+}
+
+function onGetCorrecto($) {
+    return function(respuesta) {
+        var productos = respuesta._embedded.productos;
+        console.log(productos);
+        $(productos).each(porCadaProducto($));
+    };
+}
+
+function porCadaProducto($) {
+    return function() {
+        console.log(this);
+        addProductoAListado($, this);
+    };
 }
 
 function addProductoAListado($, producto) {
-	$('#productos').append($('<li>').html(
-		producto.nombre + ' ' + producto.descripcion + ' ' + producto.precio));
+	var li$ = $('<li>');
+	
+	li$.html(
+		producto.nombre + ' ' + producto.descripcion + ' ' + producto.precio
+	);
+	
+	li$.appendTo('#productos');
 }
