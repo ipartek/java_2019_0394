d0be55cd8bdc721f74fa8af6a03fa0b623a2b1af
Javier Lete <jglete@gmail.com>
2019-11-07 09:16:32 +0100

    Ejemplo Completo con Spring Starter Template

---
+++ MF0968_3/ejemplocompleto/src/main/java/com/ipartek/formacion/ejemplocompleto/EjemplocompletoApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplocompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplocompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplocompletoApplication.class, args);
+	}
+
+}
---
+++ MF0968_3/ejemplocompleto/src/test/java/com/ipartek/formacion/ejemplocompleto/EjemplocompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplocompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplocompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
